db788ebcc78c752dc0e5c3559548da281272439b
jeff.choi <rs1017@naver.com>
2026-02-14 03:58:18 +0900

    1
    1

--- godot_setup_link.bat
+++ godot_setup_link.bat
@@ -1,30 +1,40 @@
 @echo off
-SET PROJECT_DIRECTORY=%~dp0
-pushd %PROJECT_DIRECTORY%
+cd /d "%~dp0"
 
-:: ========================================================
-:: 1. reference 폴더 링크 (공용 리소스)
-:: ========================================================
-if exist reference rmdir reference
-mklink /d reference ..\reference
-
-:: ========================================================
-:: 2. Engine 링크 생성 (핵심!)
-:: 긴 엔진 폴더명을 'Engine'이라는 짧은 이름으로 연결합니다.
-:: ========================================================
-if exist Engine rmdir Engine
-mklink /d Engine ..\Godot_v4.5.1-stable_mono_win64
-
-:: ========================================================
-:: 3. godot-game (상위 루트) 링크
-:: ========================================================
-if exist godot-game rmdir godot-game
-mklink /d godot-game ..
-
-popd
+echo ============================================
+echo Current directory: %cd%
+echo ============================================
 echo.
-echo [설정 완료] 
-echo 1. reference  -> ..\reference
-echo 2. Engine     -> ..\Godot_v4.5.1-stable_mono_win64
-echo 3. godot-game -> ..
+
+:: 1. reference
+echo [1] reference
+if exist reference (
+    del /f /q reference 2>nul
+    rmdir reference 2>nul
+)
+mklink /d reference ..\reference
+echo.
+
+:: 2. Engine
+echo [2] Engine
+if exist Engine (
+    del /f /q Engine 2>nul
+    rmdir Engine 2>nul
+)
+mklink /d Engine ..\Godot_v4.5.1-stable_mono_win64
+echo.
+
+:: 3. godot-game
+echo [3] godot-game
+if exist godot-game (
+    del /f /q godot-game 2>nul
+    rmdir godot-game 2>nul
+)
+mklink /d godot-game ..
+echo.
+
+echo ============================================
+echo [VERIFY]
+dir /al 2>nul | findstr "<"
+echo ============================================
 pause
